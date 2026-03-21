28f10e965215012a8dc47cdcd647806c99381673
Felipe Zani <amanciofzani@gmail.com>
2026-03-21 11:25:51 +0100

    feature/E2Q1Achieved

--- GiantComponents/gnp_generator.py
+++ GiantComponents/gnp_generator.py
@@ -1,44 +1,53 @@
 import random as rd 
 import math
+
 def draw_skip_distance(p):
     if(p == 0):
-        return float("inf")
+        return -1 
     if(p == 1):
-        return 1
+        return 0
     
     u = rd.random()
-    jump_val = math.ceil((math.log(1-u)-1)/math.log(1-p))
+    jump_val = math.ceil((math.log(1-u))/math.log(1-p))
     return jump_val
     
-def append_edge(E,i,j):
+def generate_GNP_graph(n, p) :
+    E = {i+1 : set() for i in range(n)}
     
-    if(i == j):
-       return E;
-    if(i+1 not in E.keys()):
-        E[i+1]=[j+1,]
-    else:
-        E[i+1].append(j+1)
-    if(j+1 not in E.keys()):
-       E[j+1]=[i+1,]
-    else:
-        E[j+1].append(i+1)
-    return E
-
-def generate_graph(n, p):
-    E = {}
-
     k = -1
+    total_possible_edges = int(n * (n - 1)//2)
+    # The bellow code is slower than the uncommented code
+    # while True:
+    #
+    #     l = draw_skip_distance(p)
+    #     if (l == float("inf")):
+    #         return E;
+    #     k = k+l+1
+    #     if k < total_possible_edges:
+    #         i = math.floor((1+math.sqrt(1+8*k))*0.5) #Solving i for the triangular matrix
+    #         j = k - int(i*(i-1)*0.5) 
+    #         E[i+1].add(j+1)
+    #         E[j+1].add(i+1)
+    #     else:
+    #         return E
+
+    i = 1             # Current row index
+    row_boundary = 0  # Total edges in rows BEFORE row i
+
     while True:
-        
         l = draw_skip_distance(p)
-        if (l == float("inf")):
-            return;
-        k = k+l+1
-        if k < n*n:
-            i,j = math.floor(k/n),k%n
-            print(f"{i+1} and {j+1}")
-            E = append_edge(E,i,j) # Adjacency Matrix doesnt scale up well
-        else:
+        k = k + l + 1
+        if k >= total_possible_edges:
             return E
 
-print(generate_graph(3,0.5))
+        #Increment i whenever k exceeds the current row
+        # A row 'i' has exactly 'i' edges (0, 1, ..., i-1)
+        while k >= row_boundary + i:
+            row_boundary += i
+            i += 1
+
+        j = k - row_boundary
+        v_i,v_j = i+1,j+1 
+        E[v_i].add(v_j)
+        E[v_j].add(v_i)
+generate_GNP_graph(15000,0.5)
